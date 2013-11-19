a5bdc9a32a8d913ae6ace6967ba17cddcddfcd9c
Graham Smith <graham@thingie.co.uk>
2013-11-19 11:05:20 +0000

    Split the view page from the new page.
    - Fixed an issue where extra form fields caused a click event to propagate instead of submit.
    - Parsing the time from the string.

--- _assets/js/kairos.js
+++ _assets/js/kairos.js
@@ -5,9 +5,17 @@
 'use strict'
 
 var chunk = {
+    order:      0,
+    type:       '',
     text:       '',
     position:   0,
     endTime:    ''
+}
+
+var list_obj = {
+    title:      '',
+    end_time:   '',
+    chunks:     []
 }
 
 var Kairos = function() {
@@ -24,6 +32,11 @@ var Kairos = function() {
         lists = JSON.parse( localStorage.getItem('lists') ).lists;
     }
 
+    this.deleteStorage = function() {
+        var empty = [];
+        localStorage.setItem('lists', JSON.stringify({ lists: empty }));
+    }
+
     var updateStorage = function() {
         localStorage.setItem('lists', JSON.stringify({ lists: lists }));
     }
@@ -38,22 +51,12 @@ var Kairos = function() {
         // Is there an element with a class of template? If so... ACTION.
         if( $2('.template') ) {
 
+            // Submit Events
             $2('.template').addEventListener('submit', function(e) {
                 
                 // What has been evented?
                 var t = e.target.className;
                 
-                // List form
-                if(t == 'add-list-form') {
-                    var title = $2('.add-list-form [name="list"]').value;
-
-                    self.current_list = title;
-                    self.storeList(title);
-
-                    $2('.template').innerHTML = $2('.add-first-chunk-template').cloneNode().innerHTML;
-                    $2('.editing-list').innerHTML = self.current_list;
-                }
-
                 // Chunk Form
                 if(t == 'add-chunk-form') {
 
@@ -95,6 +98,36 @@ var Kairos = function() {
                 e.preventDefault();
 
             }, false);
+
+            // Click events
+            $2('.template').addEventListener('click', function(e) {
+
+                var t = e.target.className;
+            
+                // List form
+                if(t == 'begin') {
+                    var title   = $2('.add-list-form [name="list"]').value;
+                    var endtime = $2('.add-list-form [name="endtime"]').value;
+
+                    var new_list = clone(list_obj);
+
+                    new_list.title      = title;
+                    new_list.endtime    = endtime;
+
+                    self.current_list = title;
+                    self.storeList(new_list);
+
+                    $2('.template').innerHTML       = $2('.add-first-chunk-template').cloneNode().innerHTML;
+                    $2('.editing-list').innerHTML   = self.current_list;
+                }
+
+                if(t == 'all-done') {
+                    window.location = e.target.href + '?list=' + encodeURI(self.current_list);
+                }
+
+                e.preventDefault();
+
+            }, false);
             
         }
     }
@@ -115,9 +148,9 @@ var Kairos = function() {
     /*
         Store Chunk
     */
-    this.storeList = function(title) {
+    this.storeList = function(list) {
 
-        lists.push( { title: title } );
+        lists.push( list );
 
         updateStorage();
     }
@@ -137,6 +170,8 @@ var Kairos = function() {
         return false;
     }
 
+    /*
+    */
     this.storeEndtime = function(title, endtime) {
         for (var i = 0; i < lists.length; i++) {
             if( lists[i].title == title ) {
@@ -168,13 +203,19 @@ var Kairos = function() {
 
         for (var i = 0; i < lists.length; i++) {
             if( lists[i].title == title ) {
-                if( lists[i].chunks ) {
 
-                    chunk.position = parseInt(this.getMaxChunkPosition(title)) + parseInt(inc);
+                if( lists[i].chunks ) {
+                    chunk.order     = lists[i].chunks.length + 1;
+                    chunk.type      = inc == 1 ? 'then' : 'meanwhile';
+                    chunk.position  = parseInt(this.getMaxChunkPosition(title)) + parseInt(inc);
+
                     lists[i].chunks.push( chunk );
                 }
                 else {
-                    chunk.position = 1;
+                    chunk.order     = 1;
+                    chunk.type      = 'first';
+                    chunk.position  = 1;
+
                     lists[i].chunks = [ chunk ];
                 }
 
@@ -185,6 +226,8 @@ var Kairos = function() {
 
     }
 
+    /*
+    */
     this.getMaxChunkPosition = function(title) {
         var maxPos = 0;
 
@@ -209,7 +252,7 @@ var Kairos = function() {
     /*
         Show Lists
     */
-    this.showListChunks = function(title) {
+    this.showListChunks = function(title, parse) {
         var chunk_html = '';
 
         for (var i = 0; i < lists.length; i++) {
@@ -218,7 +261,20 @@ var Kairos = function() {
 
                 for (var i = 0; i < chunks.length; i++) {
                     var ct = $2('.chunk-template').cloneNode();
-                    ct.querySelector('.text').innerHTML = chunks[i].text;
+
+                    ct.querySelector('li').setAttribute('class', chunks[i].type);
+                    ct.querySelector('.type').innerHTML = chunks[i].type;
+
+                    if( parse ) {
+                        var parsed = this.parseString(chunks[i].text);
+
+                        ct.querySelector('.text').innerHTML     = parsed[0];
+                        ct.querySelector('.duration').innerHTML = parsed[1]+'m';
+                    }
+                    else {
+                        ct.querySelector('.text').innerHTML = chunks[i].text;
+                    }
+                    
                     chunk_html += ct.innerHTML;
                 }
             }
@@ -228,7 +284,41 @@ var Kairos = function() {
         $2('.chunk-list').innerHTML = '<ul>'+ chunk_html + '</ul>';
     }
 
+    /*
+    */
+    this.parseString = function(str) {
+        var new_str         = str.toLowerCase();
+        var split           = new_str.split('for');
 
+        if( split.length <= 1 )
+            return [split[0], '0'];
+
+        var activity        = split[0];
+        var duration_string = split[1];
+
+        var numberwang_array    = ['one','two','three','four','five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
+        var numbers_array       = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,30,40,50,60,70,80,90];
+
+        for (var i = 0; i < numberwang_array.length; i++) {
+            if( duration_string.indexOf(numberwang_array[i]) != -1 ) {
+                duration_string = duration_string.replace(numberwang_array[i], numbers_array[i]);
+            }
+        }
+
+        var duration_int = parseInt(duration_string);
+
+        if( duration_string.match(/h/) ) {
+            duration_int = duration_int * 60;
+        }
+        else if( duration_string.match(/d/) ) {
+            duration_int = duration_int * 60 * 24;
+        }
+
+        return [activity, duration_int];
+    }
+
+
+    /* Release the kraken! */
     init();
 
 };
