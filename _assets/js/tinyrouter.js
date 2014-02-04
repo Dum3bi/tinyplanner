a9e6d6859c486fec6f701381c172caddf0c4be03
Graham Smith <graham@thingie.co.uk>
2014-02-04 10:34:19 +0000

    Now checking for document.querySelector and not window.queryselector. Silly me. Font sizes now only increase up to 60em, and only the main element, not the body.

--- _assets/js/tinyrouter.js
+++ _assets/js/tinyrouter.js
@@ -16,8 +16,8 @@ var TinyRouter = function(routes) {
 TinyRouter.prototype.init = function() {
     var self = this;
 
-    // Seriously, if the browser can't support this function, it's not worth bothering with.
-    if( !window.querySelector ) {
+    // Seriously, if the browser can't support this function then it's not worth bothering with.
+    if( !document.querySelector ) {
         document.body.innerHTML = '<div style="padding: 30px 15px"><h1>Tiny Planner</h1><h2>Whoa, whoa... WHOA.</h2><p>This browser is ooooooooold. And, subsequently, unsupported.</p></div>';
         return;
     }
