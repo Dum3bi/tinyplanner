e887dedbf367cc0704d84c9839e39e22b163aec3
Graham Smith <graham@thingie.co.uk>
2014-02-07 13:14:21 +0000

    Altered some of the Y movement calculations where rotations are involved. Distance is sin-based so rotating elements arrive with distance travelled

--- interactions/_assets/js/main.js
+++ interactions/_assets/js/main.js
@@ -80,10 +80,13 @@ Hammer($plans[0]).on('release touch drag pinch', function(e) {
         $newplan.removeClass('animate');
 
         var deltaY = (e.gesture.deltaY < 0) ? 0 : (e.gesture.deltaY / 2);
-            deltaY = deltaY > stepheight ? stepheight : deltaY;
+            deltaY = deltaY > 90 ? 90 : deltaY;
 
-        $plans.css("transform", "translate3d(0,"+ (deltaY - stepheight) +"px,0)");
-        $newplan.css("transform", "perspective(400px) rotate3d(1,0,0,"+ (90 - (deltaY * (90 / stepheight))) +"deg)");
+        var toRadian = Math.PI / 180;
+        var angleInRadians = deltaY * toRadian;
+
+        $newplan.css("transform", "perspective(500) rotateX("+ (90 - deltaY) +"deg)");
+        $plans.css("transform", "translate3d(0,"+ ((50 * Math.sin(angleInRadians)) - stepheight) +"px,0)");
     }
 
     if(e.type == 'pinch') {
@@ -102,15 +105,12 @@ Hammer($plans[0]).on('release touch drag pinch', function(e) {
             }
 
             deltaStart = (touch_lower - touch_upper);
-
             var midpoint = touch_upper + (deltaStart / 2);
 
             $('.step').each(function() {
                 if( $(this).offset().top < midpoint )
                     $step_break = $(this);
             });
-
-            // $step_break.css('border', '1px solid red');
         }
 
         $step_break.removeClass('animate');
@@ -129,20 +129,24 @@ Hammer($plans[0]).on('release touch drag pinch', function(e) {
                 deltaLower = (drag_lower - touch_lower),
                 dragDelta  = (drag_lower - drag_upper - deltaStart);
 
-            $('.console').html( dragDelta );
+            var toRadian = Math.PI / 180;
+            var angleInRadians = deltaY * toRadian;
 
-            // $plans.css("transform", "translate3d(0,"+ ( -50 - (touch_upper - drag_upper) ) +"px,0)");
             if( (drag_lower - drag_upper) > deltaStart ) {
-                $step_break.prevUntil('.step:first-child').css("transform", "translate3d(0,"+ -deltaUpper +"px,0)");
-                $step_break.css("margin-top", deltaLower +"px");
+                $step_break.prevUntil('.step:first-child').css("transform", "translate3d(0,"+ -(50 * Math.sin(deltaUpper * toRadian)) +"px,0)");
+                $step_break.css("margin-top", (50 * Math.sin(deltaLower * toRadian)) +"px");
 
                 // the folding step;
                 var $folding_step = $('.folding-step');
 
-                if( dragDelta < 50 ) {
+                if( dragDelta < 90 ) {
                     $folding_step.css({ 'display' : 'block', 'top' : $step_break.prev().offset().top + stepheight });
-                    $folding_step.find('.part-top').css("transform", "perspective(400px) rotate3d(1,0,0,"+ (-90 + (dragDelta * (90 / 50))) +"deg)").css('background', '-webkit-linear-gradient(top, hsl(210, 14%, 41%), hsl(210, 14%, '+ (30 + ( dragDelta * (11 / 50) )) +'%))');
-                    $folding_step.find('.part-bottom').css("transform", "perspective(400px) translate3d(0,"+ (-50 + (dragDelta)) +"px,0) rotate3d(1,0,0,"+ (90 - (dragDelta * (90 / 50))) +"deg)").css('background', '-webkit-linear-gradient(top, hsl(210, 14%, '+ (30 + ( dragDelta * (11 / 50) )) +'%), hsl(210, 14%, 41%))');
+                    $folding_step.find('.part-top')
+                        .css("transform", "perspective(500px) rotate3d(1,0,0,"+ (-90 + dragDelta) +"deg)")
+                        .css('background', '-webkit-linear-gradient(top, hsl(210, 14%, 41%), hsl(210, 14%, '+ (30 + (dragDelta * ( 10 / 90 ))) +'%))');
+                    $folding_step.find('.part-bottom')
+                        .css("transform", "perspective(500px) translate3d(0,"+ (50 * Math.sin(dragDelta * toRadian) - 52) +"px,0) rotate3d(1,0,0,"+ (90 - dragDelta) +"deg)")
+                        .css('background', '-webkit-linear-gradient(top, hsl(210, 14%, '+ (30 + (dragDelta * ( 10 / 90 ))) +'%), hsl(210, 14%, 41%))');
                 }
             }
         }
